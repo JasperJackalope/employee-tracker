5a7ef1f0f34daa059b79214fc90a4a326e2dd0dd
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-14 16:14:33 -0700

    Reformated Code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
-// Create a MySQL connection pool
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
@@ -58,7 +57,7 @@ function mainMenu() {
                 mainMenu();
               });
             break;
-            case 'Add a department':
+          case 'Add a department':
                 const departmentQuestions = [
                   {
                     type: 'input',
@@ -81,7 +80,7 @@ function mainMenu() {
                       mainMenu();
                     });
                   });
-                break;
+              break;
             case 'Add a role':
                     const roleQuestions = [
                       {
@@ -119,8 +118,8 @@ function mainMenu() {
                           }
                         );
                       });
-                break;  
-                case 'Add an employee':
+              break;  
+            case 'Add an employee':
                     const employeeQuestions = [
                       {
                         type: 'input',
@@ -162,23 +161,17 @@ function mainMenu() {
                           }
                         );
                       });
-                    break;
-                  
-                    case 'Update an employee role':
-                        // Get a list of all employees
+                break;
+              case 'Update an employee role':
                         pool.query('SELECT * FROM employees', (err, results) => {
                           if (err) {
                             console.error(err);
                             return;
                           }
-                      
-                          // Create an array of choices for the user to select from
                           const employeeChoices = results.map((employee) => ({
                             name: `${employee.first_name} ${employee.last_name}`,
                             value: employee.id,
                           }));
-                      
-                          // Prompt the user to select an employee to update
                           inquirer
                             .prompt({
                               type: 'list',
@@ -187,7 +180,6 @@ function mainMenu() {
                               choices: employeeChoices,
                             })
                             .then((employeeAnswer) => {
-                              // Prompt the user to enter the new role ID for the selected employee
                               inquirer
                                 .prompt({
                                   type: 'input',
@@ -197,8 +189,6 @@ function mainMenu() {
                                 .then((roleAnswer) => {
                                   const { employeeId } = employeeAnswer;
                                   const { newRoleId } = roleAnswer;
-                      
-                                  // Update the employee's role in the database
                                   pool.query(
                                     'UPDATE employees SET role_id = ? WHERE id = ?',
                                     [newRoleId, employeeId],
@@ -215,14 +205,11 @@ function mainMenu() {
                                 });
                             });
                         });
-                        break;
-                      
+                        break;        
           case 'Exit':
             console.log('Goodbye!');
             process.exit();
         }
       });
   }
-  
-  // Start the application by displaying the main menu
-  mainMenu();
+    mainMenu();
