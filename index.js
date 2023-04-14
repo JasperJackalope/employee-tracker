4a2a8802b0dfd3d1d6f6409b4086b83184256f11
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-13 18:46:45 -0700

    Expanded Inquirer Functionality

--- index.js
+++ index.js
@@ -58,15 +58,112 @@ function mainMenu() {
                 mainMenu();
               });
             break;
-          case 'Add a department':
-            // TODO: Handle add a department option
-            break;
-          case 'Add a role':
-            // TODO: Handle add a role option
-            break;
-          case 'Add an employee':
-            // TODO: Handle add an employee option
-            break;
+            case 'Add a department':
+                const departmentQuestions = [
+                  {
+                    type: 'input',
+                    name: 'name',
+                    message: 'Enter the name of the department:',
+                  },
+                ];
+                inquirer
+                  .prompt(departmentQuestions)
+                  .then((answers) => {
+                    const { name } = answers;
+              
+                    pool.query('INSERT INTO departments SET ?', { name }, (err, results) => {
+                      if (err) {
+                        console.error(err);
+                        return;
+                      }
+              
+                      console.log(`Department "${name}" has been added with ID ${results.insertId}`);
+                      mainMenu();
+                    });
+                  });
+                break;
+            case 'Add a role':
+                    const roleQuestions = [
+                      {
+                        type: 'input',
+                        name: 'title',
+                        message: 'Enter the title of the role:',
+                      },
+                      {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'Enter the salary of the role:',
+                      },
+                      {
+                        type: 'input',
+                        name: 'department_id',
+                        message: 'Enter the department ID of the role:',
+                      },
+                    ];
+                    inquirer
+                      .prompt(roleQuestions)
+                      .then((answers) => {
+                        const { title, salary, department_id } = answers;
+                  
+                        pool.query(
+                          'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)',
+                          [title, salary, department_id],
+                          (err, results) => {
+                            if (err) {
+                              console.error(err);
+                              return;
+                            }
+                  
+                            console.log(`Role "${title}" has been added with ID ${results.insertId}`);
+                            mainMenu();
+                          }
+                        );
+                      });
+                break;  
+                case 'Add an employee':
+                    const employeeQuestions = [
+                      {
+                        type: 'input',
+                        name: 'first_name',
+                        message: 'Enter the first name of the employee:',
+                      },
+                      {
+                        type: 'input',
+                        name: 'last_name',
+                        message: 'Enter the last name of the employee:',
+                      },
+                      {
+                        type: 'input',
+                        name: 'role_id',
+                        message: 'Enter the role ID of the employee:',
+                      },
+                      {
+                        type: 'input',
+                        name: 'manager_id',
+                        message: 'Enter the manager ID of the employee:',
+                      },
+                    ];
+                    inquirer
+                      .prompt(employeeQuestions)
+                      .then((answers) => {
+                        const { first_name, last_name, role_id, manager_id } = answers;
+                  
+                        pool.query(
+                          'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+                          [first_name, last_name, role_id, manager_id],
+                          (err, results) => {
+                            if (err) {
+                              console.error(err);
+                              return;
+                            }
+                  
+                            console.log(`Employee "${first_name} ${last_name}" has been added with ID ${results.insertId}`);
+                            mainMenu();
+                          }
+                        );
+                      });
+                    break;
+                  
           case 'Update an employee role':
             // TODO: Handle update an employee role option
             break;
